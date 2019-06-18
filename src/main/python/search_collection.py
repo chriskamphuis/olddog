d0f2c656975b1dddfa4822826c869ce5e0540e9a
chriskamphuis <mail@chriskamphuis.com>
2019-06-18 20:49:26 +0200

    make conjunctive optional

--- src/main/python/search_collection.py
+++ src/main/python/search_collection.py
@@ -18,15 +18,19 @@ class SearchCollection:
                 term_tf.tf, df, (log((528155-df+0.5)/(df+0.5))*((term_tf.tf*(1.2+1)/
                 (term_tf.tf+1.2*(1-0.75+0.75*(len/188.33)))))) AS subscore 
                 FROM (SELECT termid, docid, count as tf FROM qterms) AS term_tf 
-                JOIN (SELECT docid FROM qterms 
-                    GROUP BY docid HAVING COUNT(distinct termid) = {}) 
+                JOIN (SELECT docid FROM qterms
+                    GROUP BY docid {})
                     AS cdocs ON term_tf.docid = cdocs.docid 
-                JOIN docs ON term_tf.docid = docs.id 
-                JOIN dict ON term_tf.termid = dict.termid) 
-            SELECT scores.collection_id, score FROM (SELECT collection_id, sum(subscore) AS score 
+                JOIN docs ON term_tf.docid = docs.id
+                JOIN dict ON term_tf.termid = dict.termid)
+            SELECT scores.collection_id, score FROM (SELECT collection_id, sum(subscore) AS score
                 FROM subscores GROUP BY collection_id) AS scores JOIN docs ON 
                 scores.collection_id=docs.collection_id ORDER BY score DESC;
         """
+        conjunctive = 'HAVING COUNT(distinct termid) = {}'
+        if self.args.disjunctive:
+            conjunctive = ''
+        queryTemplate.join('{}', conjunctive)
         return queryTemplate
 
     def search(self):
@@ -73,6 +77,7 @@ class SearchCollection:
         parser = argparse.ArgumentParser()
         parser.add_argument('--filename', required=True, help='Topics file')
         parser.add_argument('--output', required=True, help='filename for output')
+        parser.add_argument('--disjunctive', required=False, action='store_true', help='disjunctive processing instead of conjunctive')
         self.args = parser.parse_args()
         self.cursor = self.getConnectionCursor()
         self.topicReader = TopicReader(self.args.filename)  
